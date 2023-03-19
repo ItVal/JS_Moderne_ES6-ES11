39a6f39b32984e5c3b3f9aa5ae0a0792abbf8bbc
ItVal <valentinhonvj1@gmail.com>
2023-03-19 20:28:34 +0100

    utilisation de template htmln

--- Chapitre01_Syntax_Types/03_Tamplete_String.js
+++ Chapitre01_Syntax_Types/03_Tamplete_String.js
@@ -5,5 +5,13 @@ let adressMail = 'valentinhonvj89@gmail.com'
 
 console.log('Votre nom est '+ nomA+ ' et votre adresse mail est '+ adressMail);
 
-//templete string
+//template string
 console.log(`Votre nom est ${nomA} et votre adresse mail est ${adressMail}`);
+
+//template html
+let htmlTemplate = `
+    <h2>Votre nom est ${nomA}</h2>
+    <span>et votre adresse mail est</span>
+    <p>${adressMail}</p>
+`;
+console.log(htmlTemplate);
