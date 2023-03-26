d591e77e46cdae33fde82801914c6c8a593a2daa
ItVal <valentinhonvj1@gmail.com>
2023-03-26 23:34:07 +0200

    feat utisation dun boulean avec les operations de comparaison

--- Chapitre01_Syntax_Types/05_Boulean.js
+++ Chapitre01_Syntax_Types/05_Boulean.js
@@ -8,3 +8,23 @@ let adressMail = "valnas@gmail.com";
 //vérifions si sur cette adresse on y trouve @
 console.log(adressMail.includes("@")); //sortie true
 console.log(adressMail.includes("x")); //sortie false
+
+//manipulation des opérateurs de comparaison 
+const age = 28
+let nomV = 'Nas'
+
+//égalité
+console.log(age == 28);// true
+//différent
+console.log(age != 28); //false
+//superiorité 
+console.log(age > 28);//false
+//superieur ou egal
+console.log(age >= 28); //true
+//infériorité
+console.log(age < 28); //false
+//inferieur ou egal
+console.log(age <= 28); //true
+
+
+
