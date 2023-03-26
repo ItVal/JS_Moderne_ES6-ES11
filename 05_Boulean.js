823baf6465a346410516ed6f00767541ba155287
ItVal <valentinhonvj1@gmail.com>
2023-03-26 23:27:27 +0200

    feat methode pouvant retourner un boulean

--- 05_Boulean.js
+++
@@ -1,3 +0,0 @@
-//variable de type boulean nous permet de faire la manipulation de vrai ou faux
-
-console.log(true, false);
---
+++ Chapitre01_Syntax_Types/05_Boulean.js
@@ -0,0 +1,10 @@
+//variable de type boulean nous permet de faire la manipulation de vrai ou faux
+
+console.log(true, false);
+
+//méthode pouvant nous retourner un boulean
+let adressMail = "valnas@gmail.com";
+
+//vérifions si sur cette adresse on y trouve @
+console.log(adressMail.includes("@")); //sortie true
+console.log(adressMail.includes("x")); //sortie false
