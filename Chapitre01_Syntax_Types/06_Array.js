89ff4cbc6650aefe39100c0b472206fef26f84d4
ItVal <valentinhonvj1@gmail.com>
2023-03-28 21:31:14 +0200

    feat tableau des numbers

--- Chapitre01_Syntax_Types/06_Array.js
+++ Chapitre01_Syntax_Types/06_Array.js
@@ -1,3 +1,7 @@
 // Arrays (tableau), en js le tableau est une collection des donnée du type {objet}
-const arrays = ['val', 'Nas', 'Amisi', 'Shab']
-console.log(arrays);
+const arrays = ['val', 'Nas', 'Amisi', 'Shab'] //tableau de strings
+console.log(arrays[3]);
+
+const tab = [1, 10, 90] //tableau de number
+console.log(tab.length);
+
