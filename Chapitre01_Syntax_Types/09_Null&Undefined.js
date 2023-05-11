9544b1a888a33198eaf917077a21e43ede49e3ff
ItVal <valentinhonvj1@gmail.com>
2023-05-11 21:12:35 +0200

    feat undefined

--- Chapitre01_Syntax_Types/09_Null&Undefined.js
+++ Chapitre01_Syntax_Types/09_Null&Undefined.js
@@ -3,3 +3,8 @@
 //Null : un variable déclaré avec une valeur null
 let myNull = null;
 console.log(myNull);
+
+//Undefined : un variable utilisé mais sans être initialisé
+let myUndefined;
+console.log(myUndefined);
+console.log(myUndefined + 10);
