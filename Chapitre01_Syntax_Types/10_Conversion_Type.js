8fa376c9da31ee26baba0a1cc0267a252928d363
ItVal <valentinhonvj1@gmail.com>
2023-05-17 23:31:10 +0200

    feat conversion string to number

---
+++ Chapitre01_Syntax_Types/10_Conversion_Type.js
@@ -0,0 +1,6 @@
+//conversion string en numbre
+let myString = '100'
+console.log(myString + 1);
+let myStringConversion = Number(myString);
+console.log(typeof myStringConversion);
+console.log(myStringConversion + 2);
