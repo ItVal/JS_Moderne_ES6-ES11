72f975ea27ea3acc9d7c9ad68f17da41e4c03636
ItVal <valentinhonvj1@gmail.com>
2023-05-17 23:35:10 +0200

    feat conversion number to string

--- Chapitre01_Syntax_Types/10_Conversion_Type.js
+++ Chapitre01_Syntax_Types/10_Conversion_Type.js
@@ -1,6 +1,13 @@
 //conversion string en numbre
 let myString = '100'
-console.log(myString + 1);
+console.log(myString + 1); //donne 1001
 let myStringConversion = Number(myString);
 console.log(typeof myStringConversion);
-console.log(myStringConversion + 2);
+console.log(myStringConversion + 2); //donne 102
+
+//conversion number en string
+let myNumber = 60;
+console.log(myNumber + 1); //donne 61
+let myNumberConversion = String(myNumber);
+console.log(typeof myNumberConversion);
+console.log(myNumberConversion + 1); //donne 601
