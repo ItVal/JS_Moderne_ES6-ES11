f59bcd0c8872b5a25dcf3926646ad121cc2d4606
ItVal <valentinhonvj1@gmail.com>
2023-05-18 22:06:19 +0200

    feat parcourir les elements du tableau avec la boucle for

--- Chapitre02_Control_flow/01_Boucles_For.js
+++ Chapitre02_Control_flow/01_Boucles_For.js
@@ -4,3 +4,10 @@
 for (let i=0; i<10; i++){
     console.log("Repete moi i jusqu'à :", i); //repète ce message de 0 jusqu'à 9 (10 fois donc)
 }
+
+//parcourir les element d'une liste (tableau) avec la boucle for
+let names = ["Val", "Nas", "Ly", "Angela", "Jacques"];
+//percourons les elements de ce tableau avec la boucle for
+for(let i = 0; i < names.length; i++){
+    console.log(i, names[i]);  //affichage l'index du tableau + l'élément de cet index
+}
