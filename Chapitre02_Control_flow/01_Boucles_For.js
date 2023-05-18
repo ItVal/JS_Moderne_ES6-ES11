4b2435fe88593ff17b1ac05d4e2090be121655e6
ItVal <valentinhonvj1@gmail.com>
2023-05-18 22:00:15 +0200

    feat boucle for

---
+++ Chapitre02_Control_flow/01_Boucles_For.js
@@ -0,0 +1,6 @@
+//Boucle ou loop en anglais, est une structure de control qui permet de repeter un bloc de code
+
+//Boucle For
+for (let i=0; i<10; i++){
+    console.log("Repete moi i jusqu'à :", i); //repète ce message de 0 jusqu'à 9 (10 fois donc)
+}
