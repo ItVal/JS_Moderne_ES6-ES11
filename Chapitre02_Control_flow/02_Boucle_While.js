74bd91983dfee36ac184898576513023f9c5b35a
ItVal <valentinhonvj1@gmail.com>
2023-05-19 23:13:40 +0200

    feat boucle while

---
+++ Chapitre02_Control_flow/02_Boucle_While.js
@@ -0,0 +1,8 @@
+//Boucle While()
+
+let i = 0 //ici le conteur est initialisé en dehors de la boucle
+
+while(i<5){
+    console.log("imprime moi le nombre de fois du conteur", i);
+    i++ //l'incrémentation aussi ça se fait à l'interieur de la boucle plutôt sur la condition
+}
