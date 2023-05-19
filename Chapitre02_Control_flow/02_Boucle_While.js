535f0dacb005e7aa6a793d13df11c1fba9e2a222
ItVal <valentinhonvj1@gmail.com>
2023-05-19 23:22:24 +0200

    feat parcourir les elements du tableau avec la boucle while

--- Chapitre02_Control_flow/02_Boucle_While.js
+++ Chapitre02_Control_flow/02_Boucle_While.js
@@ -6,3 +6,14 @@ while(i<5){
     console.log("imprime moi le nombre de fois du conteur", i);
     i++ //l'incrémentation aussi ça se fait à l'interieur de la boucle plutôt sur la condition
 }
+
+
+//Parcourir une liste (tableau) d'élément avec la boucle while
+
+let myList = ['Nas', 'Val', 'Jacques', 'Angelas']
+let j = 0; //notre conteur
+while( j < myList.length){
+    console.log(j, myList[j]);
+    j++
+}
+
