419d6f63bb968403ad2e28b628e0b0a9d6edd6bc
ItVal <valentinhonvj1@gmail.com>
2023-05-22 22:24:18 +0200

    feat concatenantion avec && logique

--- Chapitre02_Control_flow/03_If_Statement.js
+++ Chapitre02_Control_flow/03_If_Statement.js
@@ -1,1 +1,15 @@
 //Déclaration conditionnelle avec if/else 
+let age = 0;
+
+//concatenantion d'une suite conditionnelle avec &&
+
+if (age > 1 && age <= 17) {
+    console.log("vous êtes mineur");
+}else if (age > 18 && age < 50){
+    console.log("vous êtes adulte");
+}else if (age <1){
+    console.log("vous êtes bébé");
+}else{
+    console.log("vous êtes vieux");
+
+}
