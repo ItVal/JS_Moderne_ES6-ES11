9054a24950460886fcac7ff61db1592021b1be3f
ItVal <valentinhonvj1@gmail.com>
2023-05-23 19:31:13 +0200

    feat clarify same think

--- Chapitre01_Syntax_Types/09_Null&Undefined.js
+++ Chapitre01_Syntax_Types/09_Null&Undefined.js
@@ -2,9 +2,11 @@
 
 //Null : un variable déclaré avec une valeur null
 let myNull = null;
-console.log(myNull);
+console.log(myNull); //null
 
 //Undefined : un variable utilisé mais sans être initialisé
 let myUndefined;
-console.log(myUndefined);
-console.log(myUndefined + 10);
+console.log(myUndefined); //undefined
+
+//si on essaie d'utiliser ce variable alors qu'on l'a pas initialisé, nous aurons comme msg "NaN" is not a number.
+console.log(myUndefined + 10); //NaN
