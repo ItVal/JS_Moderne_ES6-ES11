d5aa76290bcebd9be6d6cb83cc89634b89e267f4
ItVal <valentinhonvj1@gmail.com>
2023-05-25 22:31:32 +0200

    feat practice switch case

--- Chapitre02_Control_flow/04_Swhich_Statement.js
+++ Chapitre02_Control_flow/04_Swhich_Statement.js
@@ -1,1 +1,19 @@
 //Swich statement
+const yourPourcentage = 49;
+
+switch(yourPourcentage){
+    case 80 :
+        console.log('Vons avez optenu', yourPourcentage);
+        break;
+    case 70 :
+            console.log('Vons avez optenu', yourPourcentage);
+            break;
+    case 60 :
+             console.log('Vons avez optenu', yourPourcentage);
+              break;  
+     case 49:
+             console.log('Vons avez optenu', yourPourcentage);
+             break; 
+    default :
+        console.log('Mention non disponible');
+}
