d2934141d2c6b2703017b5ed0ae9537b47210fc1
ItVal <valentinhonvj1@gmail.com>
2023-05-26 23:51:26 +0200

    feat variable, global scope

--- Chapitre02_Control_flow/04_Swhich_Statement.js
+++ Chapitre02_Control_flow/04_Swhich_Statement.js
@@ -17,3 +17,25 @@ switch(yourPourcentage){
     default :
         console.log('Mention non disponible');
 }
+
+//secand example
+
+
+let age = 90;
+
+switch(age){
+    case 11 :
+        console.log('Vons avez optenu', yourPourcentage);
+        break;
+    case 70 :
+            console.log('Vons avez optenu', yourPourcentage);
+            break;
+    case 60 :
+             console.log('Vons avez optenu', yourPourcentage);
+              break;  
+     case 49:
+             console.log('Vons avez optenu', yourPourcentage);
+             break; 
+    default :
+        console.log('Mention non disponible');
+}
---
+++ Chapitre02_Control_flow/04_Variable_GlobalScope.js
@@ -0,0 +1,4 @@
+//global scope
+
+let myValue = 20;
+console.log("le variable  myValue est accessible en global scop", myValue);
