74ceb04f74b62b25097339e2231d2e74d22553ca
ItVal <valentinhonvj1@gmail.com>
2023-05-26 23:56:29 +0200

    feat variable, scope 2

--- Chapitre02_Control_flow/04_Variable_GlobalScope.js
+++ Chapitre02_Control_flow/04_Variable_GlobalScope.js
@@ -7,6 +7,9 @@ console.log("le variable  myValue est accessible en global scop", myValue);
 if(true){
     myValue = 100 //reafectaion d'une nouvelle valeur dans notre variable global
     console.log("scope 1", myValue);
-
+    if(true){
+        myValue = 150; //nouvelle reafectaion d'une nouvelle valeur dans notre variable global
+        console.log("je suis scope 2", myValue);
+    }
   
 }
