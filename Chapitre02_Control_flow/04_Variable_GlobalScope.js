18e2bbe2b4fbb86912581fda52d19ee1b1e8da8f
ItVal <valentinhonvj1@gmail.com>
2023-05-26 23:54:42 +0200

    feat variable, scope 1

--- Chapitre02_Control_flow/04_Variable_GlobalScope.js
+++ Chapitre02_Control_flow/04_Variable_GlobalScope.js
@@ -2,3 +2,11 @@
 
 let myValue = 20;
 console.log("le variable  myValue est accessible en global scop", myValue);
+
+//scope 1
+if(true){
+    myValue = 100 //reafectaion d'une nouvelle valeur dans notre variable global
+    console.log("scope 1", myValue);
+
+  
+}
