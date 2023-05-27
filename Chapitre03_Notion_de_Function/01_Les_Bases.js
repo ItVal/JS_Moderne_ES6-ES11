2acf5e2f14bfb87b30056c05e72f6f7310370915
ItVal <valentinhonvj1@gmail.com>
2023-05-27 23:45:24 +0200

    feat fonction avec parametre

--- Chapitre03_Notion_de_Function/01_Les_Bases.js
+++ Chapitre03_Notion_de_Function/01_Les_Bases.js
@@ -5,9 +5,17 @@ function helloWord(){
 //Après avoir déclaration une fonction, cette dernière doit être appellée pour avoir le resultat attendu
 helloWord();
 
-//Declaration fonctionnelle. Avec cette deuxième façons de declaration, on ne peut appellé notre function avant qu'elle soit déclaré comme c'était le cas de notre première function
+//Declaration fonctionnelle. Avec cette deuxième façons de declaration, on ne peut appellé notre function avant qu'elle soit déclaré comme c'était le cas de notre première function (recommendé)
 const myHelloWorld = function() {
     console.log("Hello my second function");
 }
 //Appellons notre fonction
 myHelloWorld();
+
+//Function avec paramètre 
+const myParamsFunction = function(x, y) {
+    const res = x + y;
+    console.log(`la somme de ${x} et ${y} vaut : ${res}`);
+}
+// pour une function avec des paramètre, il faut passé les valeurs de paramètre lors de son appel
+myParamsFunction(4, 10);
