db213a723dec4357d288499f701c6049b003bfbd
ItVal <valentinhonvj1@gmail.com>
2023-05-27 23:34:28 +0200

    feat declaration fonctionnelle d une fonction

--- Chapitre03_Notion_de_Function/01_Les_Bases.js
+++ Chapitre03_Notion_de_Function/01_Les_Bases.js
@@ -5,4 +5,9 @@ function helloWord(){
 //Après avoir déclaration une fonction, cette dernière doit être appellée pour avoir le resultat attendu
 helloWord();
 
-//
+//Declaration fonctionnelle. Avec cette deuxième façons de declaration, on ne peut appellé notre function avant qu'elle soit déclaré comme c'était le cas de notre première function
+const myHelloWorld = function() {
+    console.log("Hello my second function");
+}
+//Appellons notre fonction
+myHelloWorld();
