b8dfe24d787e4aefa9f215c30a4874ad0550fffb
ItVal <valentinhonvj1@gmail.com>
2023-05-27 23:30:34 +0200

    feat declaration simple d une fonction

--- Chapitre03_Notion_de_Function/01_Les_Bases.js
+++ Chapitre03_Notion_de_Function/01_Les_Bases.js
@@ -1,1 +1,8 @@
-//declaration d'une fonction
+//declaration simple d'une fonction : Avec cette façons de declaration, nous pouvons appellé notre function avont même que cette dernière ne soit déclaré
+function helloWord(){
+    console.log("Hello World");
+}
+//Après avoir déclaration une fonction, cette dernière doit être appellée pour avoir le resultat attendu
+helloWord();
+
+//
