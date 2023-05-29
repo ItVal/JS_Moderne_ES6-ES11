84a4437394d5ec076c396d8ef6b580b3f97d5270
ItVal <valentinhonvj1@gmail.com>
2023-05-29 23:11:32 +0200

    feat strure de callBack function

--- Chapitre03_Notion_de_Function/callBack_Function.js
+++ Chapitre03_Notion_de_Function/callBack_Function.js
@@ -1,1 +1,7 @@
 //En js, callBack représente une fonction passée en paramètre (argument) d'une autre fonction
+
+//structure
+const myCallBackFunction = (callbackFunct) =>{
+    let number = 40;
+    callbackFunct(number);
+}
