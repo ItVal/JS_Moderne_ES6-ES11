1f3f5c7f91e259a76ff1db15db514fc457b890f0
ItVal <valentinhonvj1@gmail.com>
2023-05-30 00:11:13 +0200

    feat utilisation de forEach avec callback séparée

--- Chapitre03_Notion_de_Function/callBack_Function.js
+++ Chapitre03_Notion_de_Function/callBack_Function.js
@@ -20,3 +20,9 @@ let tab = ["Val", "Nas", "Roy", "Safi"]
 tab.forEach((etudiant, index) => {
     console.log(index, etudiant);
 })
+
+//forEach avec un callBack séparé
+let myStack = ["react", "angular", "express", "tailwind"]
+
+const stack = (tech, index) => console.log(`${index} My favorite stack is ${tech}`);
+myStack.forEach(stack) ;
