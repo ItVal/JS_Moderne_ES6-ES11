545f602f44ec64fa9ec215d5c7f7212390b486e1
ItVal <valentinhonvj1@gmail.com>
2023-05-31 22:38:03 +0200

    feat querySelector, selection specifique id or classe

--- Chap04_DOM/01_querySelector.js
+++ Chap04_DOM/01_querySelector.js
@@ -1,5 +1,8 @@
 //utilisation de querySelector
 //selection via une balise
-const para = document.querySelector('p');  //ça selectionne le premier paragraphe du dom
-console.log(para); 
+const para = document.querySelector("p"); //ça selectionne le premier paragraphe du dom
+console.log(para);
 
+//Selection d'un élement spécifique via une classe ou id
+const specificSelect = document.querySelector(".err"); //selection via une classe
+console.log(specificSelect);
