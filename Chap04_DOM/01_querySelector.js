ee567642c63c0df4c1bce84acb242628fb1a052a
ItVal <valentinhonvj1@gmail.com>
2023-05-31 22:41:32 +0200

    feat querySelector, selection complexe

--- Chap04_DOM/01_querySelector.js
+++ Chap04_DOM/01_querySelector.js
@@ -6,3 +6,9 @@ console.log(para);
 //Selection d'un élement spécifique via une classe ou id
 const specificSelect = document.querySelector(".err"); //selection via une classe
 console.log(specificSelect);
+
+//selection complexe d'un element
+const complexeSelect = document.querySelector("div.err");
+console.log(complexeSelect);
+
+//
