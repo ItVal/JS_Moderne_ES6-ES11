0aa359092a57c25a6637b201f8bdce6ddff86d93
ItVal <valentinhonvj1@gmail.com>
2023-06-02 21:08:00 +0200

    feat HtmlCollection

--- Chap04_DOM/02_getElementBy.js
+++ Chap04_DOM/02_getElementBy.js
@@ -5,3 +5,10 @@ console.log(myTitle);
 //getElement by class name
 const myParams = document.getElementsByClassName('err');
 console.log(myParams);
+
+//HTMLCollection est un peu différent avec NodeList car il ne prend pas en charge
+//la méthode forEach
+
+//alors pour avoir accès à ces élements, on doit passé par l'utilisation des indices
+console.log(myParams[0]); //affiche le premier élément de la liste
+console.log(myParams[1]); //affiche le deuxième élément de la liste
