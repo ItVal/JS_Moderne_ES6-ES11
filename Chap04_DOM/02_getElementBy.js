c454258dcfccfe62d8ed54f4fbd66b320edf4a1d
ItVal <valentinhonvj1@gmail.com>
2023-06-02 21:03:29 +0200

    feat getElementByClassName

--- Chap04_DOM/02_getElementBy.js
+++ Chap04_DOM/02_getElementBy.js
@@ -1,3 +1,7 @@
 //getElement by id
 const myTitle = document.getElementById('title');
 console.log(myTitle);
+
+//getElement by class name
+const myParams = document.getElementsByClassName('err');
+console.log(myParams);
