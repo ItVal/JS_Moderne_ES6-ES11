4bbd96ae0fd1c5ee262b8caaaa4a0f2a080c2a37
ItVal <valentinhonvj1@gmail.com>
2023-06-02 20:44:46 +0200

    feat select all classes via querySelectorAll

--- Chap04_DOM/01_querySelector.js
+++ Chap04_DOM/01_querySelector.js
@@ -1,23 +1,30 @@
 //utilisation de querySelector
 //selection via une balise
-const para = document.querySelector("p"); //ça selectionne le premier paragraphe du dom
+const para = document.querySelector('p'); //ça selectionne le premier paragraphe du dom
 console.log(para);
 
 //Selection d'un élement spécifique via une classe ou id
-const specificSelect = document.querySelector(".err"); //selection via une classe
+const specificSelect = document.querySelector('.err'); //selection via une classe
 console.log(specificSelect);
 
 //selection complexe d'un element
-const complexeSelect = document.querySelector("div.err");
+const complexeSelect = document.querySelector('div.err');
 console.log(complexeSelect);
 
 //obtenir la ref d'un element via le navigateur
-const refNav = document.querySelector('body > h1:nth-child(2)')
+const refNav = document.querySelector('body > h1:nth-child(2)');
 console.log(refNav);
 
 //selection multiple des élements : querySelectorAll
-const params = document.querySelectorAll('p') //on selection toutes les bases p
+const params = document.querySelectorAll('p'); //on selection toutes les bases p
 console.log(params);
 
 //selection d'element dans le NodeList
-console.log(params[0]); 
+console.log(params[0]);
+
+//utilisation de forEach pour parcourir NodeList
+params.forEach((para) => console.log(para));
+
+//selectionner toutes les classes
+const allClasses = document.querySelectorAll('err');
+console.log(allClasses);
