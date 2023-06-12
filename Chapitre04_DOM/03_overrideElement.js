3f1f61006a0c0ace8cebe41d78307aef39a6ac12
ItVal <valentinhonvj1@gmail.com>
2023-06-12 22:24:54 +0200

    feat ajoutons du contenu dans une balise avec contenu existant via innerText

--- Chapitre04_DOM/03_overrideElement.js
+++ Chapitre04_DOM/03_overrideElement.js
@@ -2,6 +2,9 @@
 const para = document.querySelector('p');
 console.log(para.innerHTML); //affiche ce trouvant sur ce paragraphe (le premier p pour notre cas)
 
-//Modifions le contenu de ce paragraphe
+//Remplaçons le contenu de ce paragraphe par un autre contenu
 para.innerText = 'Bonjour le Congo, mon pays';
 console.log(para.innerText); // notre balise p n'a plus bonjour le monde comme contenu mais desormais Bonjour le congo, mon pays
+
+//Ajouter du contenu sur ce même paragraphe sans pour autant perdre l'ancien
+para.innerText += ', Merci val pour ce petit tuto';
