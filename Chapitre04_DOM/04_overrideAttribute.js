d6bb089ff53f9985979533164bb78a6827bd9108
ItVal <valentinhonvj1@gmail.com>
2023-06-27 23:35:56 +0200

    feat edit attribute of an elmt

--- Chapitre04_DOM/04_overrideAttribute.js
+++ Chapitre04_DOM/04_overrideAttribute.js
@@ -1,3 +1,8 @@
 //utilisation de getAttribute
+//Affichage d'un attribut
 const mylink = document.querySelector('a');
 console.log(mylink.getAttribute('href'));
+
+//Modification d'un attribut
+mylink.setAttribute('href', 'https://wmdrc.org');
+// console.log(link);
